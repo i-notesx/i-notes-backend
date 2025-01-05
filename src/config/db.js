999a76eb480a93d14f85f8d30c84b92ef2282d8b
lwj <jg15575446613@163.com>
2025-01-05 23:09:36 +0800

    chore: docker-compose添加mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,12 @@ import mongoose from 'mongoose'
 import env from './env.js'
 
 const uri = `${env.DB_TYPE}://${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`
+// const uri = `${env.DB_TYPE}://mongo:${env.DB_PORT}/${env.DB_NAME}`
 
 // 连接到 MongoDB
 mongoose.connect(uri)
   .then((value) => {
-    console.log('数据库连接成功', value);
+    console.log('数据库连接成功');
   })
   .catch(err => {
     console.error('数据库连接失败:', err);
